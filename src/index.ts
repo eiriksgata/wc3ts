303872b1b25e6d9665fa55cbb8bf5eecd48133dd
eiriksgata <2353686862@qq.com>
2025-10-15 15:51:04 +0800

    初始化 Players 数组，添加从玩家句柄创建 MapPlayer 实例的逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { bj_MAX_PLAYER_SLOTS } from "./globals/define";
 import { MapPlayer } from "./handles/player";
 
 export * from "./handles/index";
@@ -6,3 +7,10 @@ export * from "./utils/index";
 export * from "./globals/index";
 
 export const Players: MapPlayer[] = [];
+
+for (let i = 0; i < bj_MAX_PLAYER_SLOTS; i++) {
+  const pl = MapPlayer.fromHandle(Player(i));
+  if (pl) {
+    Players[i] = pl;
+  }
+}
